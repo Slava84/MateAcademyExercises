ca9bbefdaf8b9061e0d450b8921b93f43febea81
Slava84 <vyacheck.p@gmail.com>
2019-05-31 16:29:34 +0300

    An unnecessary line was erased and a new one added

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,11 +1,10 @@
-package may_28;
-
 import java.math.BigInteger;
 
 public class Factorial {
     public static void main(String[] args) {
         System.out.println(factorial(5));
     }
+
     private static BigInteger factorial(int value) {
         long sum = 1;
         for(int i = 1; i <= value; i++)
