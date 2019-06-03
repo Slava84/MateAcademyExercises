eb6cd9061c189cd2984c6e39ea599723398b42a3
Slava84 <vyacheck.p@gmail.com>
2019-06-03 22:18:38 +0300

    New version of Homework 2

--- src/homework_2/BitApp.java
+++ src/homework_2/BitApp.java
@@ -1,6 +1,6 @@
 package homework_2;
 
-public class Bit {
+public class BitApp {
     public static void main(String[] args) {
         System.out.println("0001 ^ 0001 = 0000 : " + flipBit(1, 1));
         System.out.println("0001 ^ 0010 = 0011 : " + flipBit(1, 2));
